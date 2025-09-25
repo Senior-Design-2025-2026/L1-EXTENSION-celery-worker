fb94cd8e81cea055772563130fa257a27099ee3a
mattnkrueger <mkrue138@gmail.com>
2025-09-25 00:49:59 -0500

    postgres with 1-worker 1-task setup

--- src/celery_app.py
+++ src/celery_app.py
@@ -1,5 +1,5 @@
 from celery import Celery
-from celery.signals import worker_process_init
+from celery.signals import worker_process_init, worker_process_shutdown
 from sqlalchemy import engine, create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from pathlib import Path
@@ -45,34 +45,35 @@ _engine = None
 _Session = None
 
 @worker_process_init.connect
-def initialize_new_worker(**kwargs):
-    global _engine, _Session
+def initialize_new_worker(**kwargs):    
+    global _engine, _Session                                    
     _engine = create_engine(DB_URL, echo=False)
-    # Base.metadata.create_all(_engine)                             # only run if tables arent already created
+    # Base.metadata.create_all(_engine)                             # @team: keep commented; only run if tables arent already created or the db gets nuked
     _Session = sessionmaker(bind=_engine, expire_on_commit=False)
 
 @celery_app.task(name="insert_record")
 def insert_record(sensor_id, timestamp, temperature_c):
     session = _Session()
     try:
-        print("")
-        print("ADDING RECORD")
         reading = Temperature(sensor_id=sensor_id, timestamp=timestamp, temperature_c=temperature_c)
-        print("reading:", reading)
         session.add(reading)
-        print("ADDED")
         session.commit()
+    except:
+        session.rollback()
+        raise
     finally:
         session.close()
 
-# TODO: move the caching code for sending emails to the web server. this makes more sense
-# as the cache can be read when triggered. this way there is only 1 worker per email.
-# @celery_app.task()
-# def send_email(addr: str, message: str):
-#     if not red.exists("emails"):
-#         emails = get_mailing_list()
-#         red.set("emails", json.dumps(emails))
-#     else:
-#         emails = json.loads(red.get("emails"))
+@celery_app.task()
+def send_email(addr: str, message: str):
+    # if not red.exists("emails"):
+    #     emails = get_mailing_list()
+    #     red.set("emails", json.dumps(emails))
+    # else:
+    #     emails = json.loads(red.get("emails"))
 
-#     print("sending emails now...")
+    print("sending emails now...")
+
+@worker_process_shutdown.connect
+def safely_destroy_worker(**_):
+    _engine.dispose()
